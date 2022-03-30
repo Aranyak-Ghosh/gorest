2ac6b04fff573d718e24398391212ceed62b70aa
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-30 12:55:48 +0400

    Added max retry setter

--- interfaces/HttpClient.go
+++ interfaces/HttpClient.go
@@ -4,6 +4,7 @@ import "github.com/Aranyak-Ghosh/gorest/types"
 
 type HttpClient interface {
 	SetBaseUrl(baseUrl string)
+	SetMaxRetry(maxRetry int)
 	Get(endpoint string, Query, headers map[string]string) *types.HttpResponse
 	Post(endpoint string, Query, headers map[string]string, body interface{}) *types.HttpResponse
 	Put(endpoint string, Query, headers map[string]string, body interface{}) *types.HttpResponse
