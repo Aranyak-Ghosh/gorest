a7d74cc53c1fb0a28e681f3f33ddcb25739c1008
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-30 20:14:17 +0400

    Updated HttpClient interface

--- interfaces/HttpClient.go
+++ interfaces/HttpClient.go
@@ -10,10 +10,10 @@ type HttpClient interface {
 	SetBaseUrl(baseUrl string) error
 	SetMaxRetry(maxRetry int)
 	SetTimeout(timeout time.Duration)
-	Del(endpoint string, query types.Query, headers map[string]string) *HttpResponse
-	Get(endpoint string, query types.Query, headers map[string]string) *HttpResponse
-	Head(endpoint string, headers map[string]string) *HttpResponse
-	Post(endpoint string, query types.Query, headers map[string]string, body any, bodyType types.ContentType) *HttpResponse
-	Put(endpoint string, query types.Query, headers map[string]string, body any, bodyType types.ContentType) *HttpResponse
-	Patch(endpoint string, query types.Query, headers map[string]string, body any, bodyType types.ContentType) *HttpResponse
+	Del(endpoint string, query types.Query, headers map[string]string) HttpResponse
+	Get(endpoint string, query types.Query, headers map[string]string) HttpResponse
+	Head(endpoint string, headers map[string]string) HttpResponse
+	Post(endpoint string, query types.Query, headers map[string]string, body any, bodyType types.ContentType) HttpResponse
+	Put(endpoint string, query types.Query, headers map[string]string, body any, bodyType types.ContentType) HttpResponse
+	Patch(endpoint string, query types.Query, headers map[string]string, body any, bodyType types.ContentType) HttpResponse
 }
