7463b81e82780d644a054e859d92d203af309220
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-03-30 11:45:39 +0400

    Added initial interface

---
+++ interfaces/HttpClient.go
@@ -0,0 +1,12 @@
+package interfaces
+
+import "github.com/Aranyak-Ghosh/gorest/types"
+
+type HttpClient interface {
+	SetBaseUrl(baseUrl string)
+	Get(endpoint string, Query, headers map[string]string) *types.HttpResponse
+	Post(endpoint string, Query, headers map[string]string, body interface{}) *types.HttpResponse
+	Put(endpoint string, Query, headers map[string]string, body interface{}) *types.HttpResponse
+	Patch(endpoint string, Query, headers map[string]string, body interface{}) *types.HttpResponse
+	Del(endpoint string, Query, headers map[string]string) *types.HttpResponse
+}
